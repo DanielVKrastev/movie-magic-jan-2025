fe57eaf548c3320035809e272a4896b8d7630b93
Daniel Krastev <danielkrastev01@gmail.com>
2025-01-15 20:50:17 +0200

    Add funtionality for add data

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -7,14 +7,26 @@ movieController.get('/create', (req, res) => {
     res.render('create');
 });
 
+movieController.post('/create', (req, res) => {
+    console.log('POST MOVIE');
+    
+    const newMovie = req.body;
+    console.log(newMovie);
+    
+
+    res.end();
+    
+})
+
 movieController.get('/:movieId/details', (req, res) => {
     const movieId = req.params.movieId;
-    //Get movie data for movieId
+    
+    //Get movie data from movieId
 
     const movie = movieService.findOne(movieId);
     console.log(movie);
     
-    res.render('details');
+    res.render('details', { movie });
 });
 
 export default movieController;
