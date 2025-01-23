ade92b6bcc2d5de29ed0061c1f3b0216f763fad3
Daniel Krastev <danielkrastev01@gmail.com>
2025-01-23 23:11:05 +0200

    Show movie in Attach Page

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -31,8 +31,13 @@ movieController.get('/:movieId/details', async (req, res) => {
     res.render('movie/details', { movie });
 });
 
-movieController.get('/:movieId/attach-cast', (req, res) => {
-    res.render('movie/attach-cast');
+movieController.get('/:movieId/attach-cast', async (req, res) => {
+    const movieId = req.params.movieId;
+
+    //Get movie data from movieId
+    const movie = await movieService.getOne(movieId);
+
+    res.render('movie/attach-cast', { movie });
 });
 
 
