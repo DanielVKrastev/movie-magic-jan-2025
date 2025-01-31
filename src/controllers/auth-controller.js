e8039572d6e30ee82d4f2406781ac2c8f5b11823
Daniel Krastev <danielkrastev01@gmail.com>
2025-01-31 23:00:17 +0200

    Add login Page

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -5,15 +5,20 @@ const authController = Router();
 
 authController.get('/register', (req, res) => {
     res.render('auth/register')
-})
+});
 
 authController.post('/register', async (req, res) => {
     const userData = req.body;
 
     await authService.register(userData);
 
-    res.end();
+    res.redirect('/auth/login');
     
-})
+});
+
+authController.get('/login', (req, res) => {
+    res.render('auth/login');
+});
+
 
 export default authController;
