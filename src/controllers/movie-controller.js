1925365e0786dfff8b7fb1f174b08c63975e48e1
Daniel Krastev <danielkrastev01@gmail.com>
2025-02-02 20:44:55 +0200

    Add edit page with hard cored data

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -72,4 +72,8 @@ movieController.get('/:movieId/delete', async (req, res) => {
    res.redirect('/');
 });
 
+movieController.get('/:movieId/edit', async (req, res) => {
+   res.render('movie/edit');
+});
+
 export default movieController;
