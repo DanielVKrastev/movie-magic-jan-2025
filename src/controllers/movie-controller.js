81bad5f9f8b12d890932634e67c88d5f084cd92a
Daniel Krastev <danielkrastev01@gmail.com>
2025-02-02 21:11:03 +0200

    Update movie from edit form

--- src/controllers/movie-controller.js
+++ src/controllers/movie-controller.js
@@ -99,4 +99,16 @@ movieController.get('/:movieId/edit', async (req, res) => {
     res.render('movie/edit', { movie, categories });
 });
 
+movieController.post('/:movieId/edit', async (req, res) => {
+    const movieData = req.body;
+    const movieId = req.params.movieId;
+
+    // TODO: Check if creator
+
+    await movieService.update(movieId, movieData);
+
+    res.redirect(`/movies/${movieId}/details`);
+
+});
+
 export default movieController;
