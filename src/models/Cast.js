7e7deb2ea64f07fd7cc694b28c276828e049f59a
Daniel Krastev <danielkrastev01@gmail.com>
2025-02-04 17:10:52 +0200

    Add validation in Cast Model

--- src/models/Cast.js
+++ src/models/Cast.js
@@ -1,10 +1,27 @@
 import { Schema, model } from "mongoose";
 
 const castSchema = new Schema({
-    name: String,
-    age: Number,
-    born: String,
-    imageUrl: String,
+    name: {
+        type: String,
+        required: true,
+        minLength: [5, 'Name should be at least 5 characters long!'],
+        maxLength: 250,
+        match: [/[A-Za-z 0-9]+$/, 'Name should be alphanumeric, digits and whitespaces only!'],
+    },
+    age: {
+        type: Number,
+        min: 0,
+        max: 120,
+    },
+    born: {
+        type: String,
+        minLength: 10,
+        match: [/[A-Za-z 0-9]+$/, 'Born should be alphanumeric, digits and whitespaces only!'],
+    },
+    imageUrl: {
+        type: String,
+        match:[/https?:\/\//, 'ImageUrl is invalid!'],
+    },
 });
 
 const Cast = model('Cast', castSchema);
